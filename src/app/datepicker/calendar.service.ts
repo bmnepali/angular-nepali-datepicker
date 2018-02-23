6ecb138e68fbcd6237e42e3b0000cbfe2b30577b
Buddha Man Nepali <bmnepali980@gmail.com>
2018-02-23 09:25:54 +0545

    NDP: Adds proper comment to the service function

--- src/app/datepicker/calendar.service.ts
+++ src/app/datepicker/calendar.service.ts
@@ -11,7 +11,10 @@ export class CalendarService {
   // Resolve HTTP using the constructor
   constructor (public http: HttpClient) {}
 
-  // private instance variable to hold base url
+  /**
+   * Gets calendar data from given url
+   * @param {String} commentsUrl 
+   */
   getCalendar(commentsUrl) {
     return this.http.get(commentsUrl);
   }
